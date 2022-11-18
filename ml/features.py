b1f218809f77098bc8d20d2b4fd11e933b1acc21
sebdodt <mats.dodt@googlemail.com>
2022-11-18 17:41:18 -0500

    added feature script and some functions

--- ml/features.py
+++ ml/features.py
@@ -1,9 +1,19 @@
-
+import pandas as pd
+import addfips
 
 def read_data():
-    pass
+    # read in features and label data
+    path = 'data/weather_data.csv'
+    df = pd.read_csv(path)
 
-
+    # create column that counts the months since January 2014
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['month'] = df['datetime'].dt.month
+    df['year'] = df['datetime'].dt.year
+    df['period'] = (df['year'] - 2014) * 12 + df['month'] - 1
+    return df
+    
 
 if __name__=='__main__':
-    print("test")
+    df = read_data()
+
